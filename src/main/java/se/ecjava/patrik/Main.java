3467119f60cbd7f007d02574b17e6c9f5274c282
PJ93 <pj93coder@gmail.com>
2019-08-21 16:07:01 +0200

    Added the basic skeleton of a calculator - PJ93

--- src/main/java/se/ecjava/patrik/Main.java
+++ src/main/java/se/ecjava/patrik/Main.java
@@ -1,4 +1,6 @@
 package se.ecjava.patrik;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
     /**
@@ -8,11 +10,34 @@ public class Main {
      *Make sure to use Test
      * -------------------------PJ93----------------------------
      */
-
+        static Scanner scanner = new Scanner(System.in);
         public static void MainCalc(){
             boolean keepalive = true;
             while(keepalive){
-                System.out.println("Enter number");
+                System.out.println("Enter the first number");
+                int number1 = Integer.parseInt(scanner.nextLine());
+                System.out.println("Enter the seconded number");
+                int number2 = Integer.parseInt(scanner.nextLine());
+                //char mathselect = Character.isJavaIdentifierPart(); deleted and replaced with a simple number option for the time be
+                System.out.println("choose math equation");
+                int mathselect = Integer.parseInt(scanner.nextLine());
+                switch(mathselect){
+                    case 1:
+                        System.out.println(number1+number2);
+                        break;
+                    case 2:
+                        System.out.println(number1-number2);
+                        break;
+                    case 3:
+                        System.out.println(number1*number2);
+                        break;
+                    case 4:
+                        System.out.println(number1%number2);
+                        break;
+                    default:
+                        System.out.println("please only use +, - , * and /");
+                        break;
+                }//switch
 
             }//while
         }//static void
