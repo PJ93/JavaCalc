8fdbfc3b4a6630287c5a13e8afe28d4b5dd8ee14
PJ93 <pj93coder@gmail.com>
2019-08-21 15:17:51 +0200

    Created the file for Java Calc assignment threw in some basic code and comments explaining the program

---
+++ src/main/java/se/ecjava/patrik/Main.java
@@ -0,0 +1,18 @@
+package se.ecjava.patrik;
+
+public class Main {
+    /**
+     * --------------Program description and goal---------------
+     *Java Calculator exercise for ECUtbildning java kurs
+     *Make simple calculator that can do basic math
+     *Make sure to use Test
+     * -------------------------PJ93----------------------------
+     */
+
+        public static void MainCalc(){
+            boolean keepalive = true;
+            while(keepalive){
+
+            }//while
+        }//static void
+}//main
