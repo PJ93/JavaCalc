986c0408d49e73b897c474ed39d248ad3910b76b
PJ93 <pj93coder@gmail.com>
2019-08-22 16:47:33 +0200

    Added working test for addition method

--- src/test/java/se/ecjava/patrik/AppTest.java
+++ src/test/java/se/ecjava/patrik/AppTest.java
@@ -1,15 +1,22 @@
 package se.ecjava.patrik;
-
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import static se.ecjava.patrik.Main.scanner;
-
 import org.junit.Test;
-
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-
+    @Test
+    public void Test() {
+        //arrange
+        int AssmentX =5;
+        int AssmentY = 10;
+        int Expected = 15;
+        //act
+        int actual = Main.calcMethodAdd(AssmentX, AssmentY);
+        //Assert
+        assertEquals(Expected, actual);
+    }
 }
